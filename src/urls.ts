26667963b8912f4efae74538e297e72bfaf9c9bb
Christian Larsen <cjl2311@gmail.com>
2022-11-24 15:54:38 -0600

    Adds global exports file to package

--- src/urls.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = `https://api.kroger.com/v1`;
-export const AUTH_URL = `${API_URL}/connect/oauth2`;
-export const TOKEN_URL = `${AUTH_URL}/token`;
-export const PRODUCT_URL = `${API_URL}/products`;
-export const LOCATION_URL = `${API_URL}/locations`;
